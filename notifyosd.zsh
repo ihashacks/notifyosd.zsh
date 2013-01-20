85ffa6ff95277fc7e4be5aac2a7e7fd200350040
Brandon Pierce <brandon@ihashacks.com>
2013-01-19 22:19:31 -0800

    cleanup. this probably should have been the first commit.

--- notifyosd.zsh
+++ notifyosd.zsh
@@ -1,24 +1,23 @@
+# end and compare timer, notify-send if needed
 function notifyosd-precmd() {
-    if [ "$cmd" != "" ]; then
+    if [ ! -z "$cmd" ]; then
         cmd_end=`date +%s`
         ((cmd_time=$cmd_end - $cmd_start))
     fi                                                                                                                              
-    if [ "$cmd" != "" -a $cmd_time -gt 10 ]; then
+    if [ ! -z "$cmd" -a $cmd_time -gt 10 ]; then
         notify-send -i utilities-terminal -u low "$cmd_basename completed" "\"$cmd\" took $cmd_time seconds"
     fi
 }
 
+# make sure this plays nicely with any existing precmd
 precmd_functions+=( notifyosd-precmd )
 
-
-function precmd() {
-    title "zsh" "%m:%55<...<%~"
-}
-
+# get command name and start the timer
 function notifyosd-preexec() {
     cmd=$1
     cmd_basename=${cmd[(ws: :)1]}
     cmd_start=`date +%s`
 }
 
+# make sure this plays nicely with any existing preexec
 preexec_functions+=( notifyosd-preexec )
