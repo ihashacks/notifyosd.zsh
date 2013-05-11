354c300bf800f11f73f22e3900fb10dde967ce67
Brandon Pierce <brandon@ihashacks.com>
2013-05-11 08:55:58 -0700

    add command completion status to notification title

--- notifyosd.zsh
+++ notifyosd.zsh
@@ -3,6 +3,7 @@ cmdignore=(vim top htop)
 
 # end and compare timer, notify-send if needed
 function notifyosd-precmd() {
+	retval=$?
     if [[ ${cmdignore[(r)$cmd_basename]} == $cmd_basename ]]; then
         return
     else
@@ -10,11 +11,16 @@ function notifyosd-precmd() {
             cmd_end=`date +%s`
             ((cmd_time=$cmd_end - $cmd_start))
         fi
+        if [ $retval -gt 0 ]; then
+			cmdstat="with warning"
+		else
+            cmdstat="successfully"
+        fi
         if [ ! -z "$cmd" -a $cmd_time -gt 10 ]; then
             if [ ! -z $SSH_TTY ] ; then
-                notify-send -i utilities-terminal -u low "$cmd_basename on `hostname` completed" "\"$cmd\" took $cmd_time seconds";
+                notify-send -i utilities-terminal -u low "$cmd_basename on `hostname` completed $cmdstat" "\"$cmd\" took $cmd_time seconds";
             else
-                notify-send -i utilities-terminal -u low "$cmd_basename completed" "\"$cmd\" took $cmd_time seconds"
+                notify-send -i utilities-terminal -u low "$cmd_basename completed $cmdstat" "\"$cmd\" took $cmd_time seconds"
             fi
         fi
         unset cmd
