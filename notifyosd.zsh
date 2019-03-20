7c4a04652457d1766b4c35f790fb5cadf1d6b81e
nanoseb <sebastien.lemaire@soton.ac.uk>
2019-03-20 15:28:59 +0000

    code clean up
    remove tab

--- notifyosd.zsh
+++ notifyosd.zsh
@@ -15,38 +15,38 @@ function active_window_id () {
 
 # end and compare timer, notify-send if needed
 function notifyosd-precmd() {
-	retval=$?
+    retval=$?
     if [[ ${cmdignore[(r)$cmd_basename]} == $cmd_basename ]] || [[ "$cmd_active_win" == "$(active_window_id)" ]]; then
         return
     else
         if [ ! -z "$cmd" ]; then
-            cmd_end=`date +%s`
+            cmd_end=$(date +%s)
             ((cmd_secs=$cmd_end - $cmd_start))
         fi
         if [ $retval -gt 0 ]; then
-			cmdstat="with warning"
-			sndstat="/usr/share/sounds/gnome/default/alerts/sonar.ogg"
-			urgency="critical"
-		else
+            cmdstat="with warning"
+            sndstat="/usr/share/sounds/gnome/default/alerts/sonar.ogg"
+            urgency="critical"
+        else
             cmdstat="successfully"
-			sndstat="/usr/share/sounds/gnome/default/alerts/glass.ogg"
-			urgency="normal"
+            sndstat="/usr/share/sounds/gnome/default/alerts/glass.ogg"
+            urgency="normal"
         fi
         if [ ! -z "$cmd" -a $cmd_secs -gt 10 ]; then
-			if [ $gnuunits -gt 0 ]; then
-				cmd_time=$(units "$cmd_secs seconds" "centuries;years;months;weeks;days;hours;minutes;seconds" | \
-						sed -e 's/\ +/\,/g' -e s'/\t//')
-			else
-				cmd_time="$cmd_secs seconds"
-			fi
+            if [ $gnuunits -gt 0 ]; then
+                cmd_time=$(units "$cmd_secs seconds" "centuries;years;months;weeks;days;hours;minutes;seconds" | \
+                        sed -e 's/\ +/\,/g' -e s'/\t//')
+            else
+                cmd_time="$cmd_secs seconds"
+            fi
             if [ ! -z $SSH_TTY ] ; then
                 notify-send -i utilities-terminal \
-						-u $urgency "$cmd_basename on `hostname` completed $cmdstat" "\"$cmd\" took $cmd_time"; \
-						play -q $sndstat
+                        -u $urgency "$cmd_basename on $(hostname) completed $cmdstat" "\"$cmd\" took $cmd_time"; \
+                        play -q $sndstat
             else
                 notify-send -i utilities-terminal \
-						-u $urgency "$cmd_basename completed $cmdstat" "\"$cmd\" took $cmd_time"; \
-						play -q $sndstat
+                        -u $urgency "$cmd_basename completed $cmdstat" "\"$cmd\" took $cmd_time"; \
+                        play -q $sndstat
             fi
         fi
         unset cmd
@@ -60,7 +60,7 @@ precmd_functions+=( notifyosd-precmd )
 function notifyosd-preexec() {
     cmd=$1
     cmd_basename=${${cmd:s/sudo //}[(ws: :)1]} 
-    cmd_start=`date +%s`
+    cmd_start=$(date +%s)
     cmd_active_win=$(active_window_id)
 }
 
