67fd7f31701b3061062bf78ad99017f226d98082
nanoseb <sebastien.lemaire@soton.ac.uk>
2019-03-20 15:38:13 +0000

    improve performances

--- notifyosd.zsh
+++ notifyosd.zsh
@@ -16,29 +16,32 @@ function active_window_id () {
 # end and compare timer, notify-send if needed
 function notifyosd-precmd() {
     retval=$?
-    if [[ ${cmdignore[(r)$cmd_basename]} == $cmd_basename ]] || [[ "$cmd_active_win" == "$(active_window_id)" ]]; then
+    if [[ ${cmdignore[(r)$cmd_basename]} == $cmd_basename ]]; then
         return
     else
         if [ ! -z "$cmd" ]; then
             cmd_end=$(date +%s)
             ((cmd_secs=$cmd_end - $cmd_start))
         fi
-        if [ $retval -gt 0 ]; then
-            cmdstat="with warning"
-            sndstat="/usr/share/sounds/gnome/default/alerts/sonar.ogg"
-            urgency="critical"
-        else
-            cmdstat="successfully"
-            sndstat="/usr/share/sounds/gnome/default/alerts/glass.ogg"
-            urgency="normal"
-        fi
-        if [ ! -z "$cmd" -a $cmd_secs -gt 10 ]; then
+
+        if [ ! -z "$cmd" -a $cmd_secs -gt 10 ] && [[ "$cmd_active_win" != "$(active_window_id)" ]]; then
+            if [ $retval -gt 0 ]; then
+                cmdstat="with warning"
+                sndstat="/usr/share/sounds/gnome/default/alerts/sonar.ogg"
+                urgency="critical"
+            else
+                cmdstat="successfully"
+                sndstat="/usr/share/sounds/gnome/default/alerts/glass.ogg"
+                urgency="normal"
+            fi
+
             if [ $gnuunits -gt 0 ]; then
                 cmd_time=$(units "$cmd_secs seconds" "centuries;years;months;weeks;days;hours;minutes;seconds" | \
                         sed -e 's/\ +/\,/g' -e s'/\t//')
             else
                 cmd_time="$cmd_secs seconds"
             fi
+
             if [ ! -z $SSH_TTY ] ; then
                 notify-send -i utilities-terminal \
                         -u $urgency "$cmd_basename on $(hostname) completed $cmdstat" "\"$cmd\" took $cmd_time"; \
